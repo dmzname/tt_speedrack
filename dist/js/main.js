1788be2d89c4bc172c638834ad143042be2af8e4
dmzname <dmz.name@gmail.com>
2024-03-01 21:24:20 +0200

    add checkboxes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+// PRODUCT CARD SLIDER
     const swiper = new Swiper(".preview__thumbnail", {
         spaceBetween: 8,
         slidesPerView: 4,
@@ -9,4 +11,50 @@ document.addEventListener("DOMContentLoaded", () => {
             swiper: swiper,
         },
     });
+
+// PLUS MINUS INPUT
+    document.querySelectorAll('[data-quantity]').forEach(button => {
+        button.addEventListener('click', incDecHandler);
+    });
+
+    function incDecHandler(e) {
+        e.preventDefault();
+        const btn = e.target.closest('button');
+        const quantityType = btn.dataset.quantity;
+        const fieldName = btn.getAttribute('data-field');
+        const input = document.querySelector(`input[name="${fieldName}"]`);
+        const step = input.dataset.step;
+        const currentVal = parseInt(input.value);
+
+        if (!isNaN(currentVal)) {
+            input.value = (quantityType === 'plus')
+                ? (currentVal + (1*step)) + '"'
+                : Math.max(currentVal - (1*step), step ? step : 0) + '"';
+        } else {
+            input.value = step ? (1*step) + '"' : 0  + '"';
+        }
+    }
+
+// CustomConveyor Belt Width is visible or not
+    const inputContainers = document.querySelectorAll('.conveyor-belt__width');
+    const custom = document.querySelector('.conveyor-belt .plus-minus-field');
+
+    inputContainers.forEach(container => {
+        const input = container.querySelector('input');
+
+        if (input) {
+            input.addEventListener('change', () => {
+                custom.hidden = !(container.classList.contains('conveyor-belt__width_custom') && input.checked);
+            });
+        }
+    });
+
+//
+    const selectBtn = document.querySelector('.additional-options__select');
+    const selectOptions = document.querySelector('.additional-options__opt');
+
+    selectBtn.addEventListener('click', function (){
+        this.classList.toggle('is-active');
+        selectOptions.hidden = !selectOptions.hidden;
+    })
 })
